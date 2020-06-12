481c59c01960fcbf8048a13eb175c99e0a6ba823
yikang <yikangchen112@gmail.com>
2020-06-12 16:50:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import axios from 'axios';
+
+export default class App extends Component {
+
+  state = {
+    fact: ''
+  }
+
+  componentDidMount() {
+    this.fetchFact();
+  }
+
+  fetchFact = async () => {
+    try {
+      const { data: { text } } = await axios.get('https://uselessfacts.jsph.pl/random.json?language=en');
+      
+      this.setState({
+        fact: text
+      })
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">
+            {this.state.fact}
+          </h1>
+          <button className="button" onClick={this.fetchFact}>Click for a new fact!</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
